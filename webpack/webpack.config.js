4f0c173ef98bdc0189b7399534ee2249aae1ad6e
joao-souza-flooxai <joao.souza.flooxai@gmail.com>
2025-05-09 13:06:28 -0300

    feat: Added img,  styles on button, and served the application with webpackserver. Finished the section.

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,18 +1,35 @@
-const webpack = require('webpack');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const modoDev = process.env.NODE_ENV !== 'production';
 
+const webpack = require('webpack');
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeAssetsPluguin = require('optimize-css-assets-webpack-plugin');
 module.exports = {
-    mode: 'development',
+    mode: modoDev ? 'development' : 'production',
     entry: './src/principal.js',
     output: {
         filename: 'principal.js',
         path: __dirname + '/public'
+    },
+    devServer: {
+        contentBase: "./public",
+        port: 9000
     },
     plugins:[
         new MiniCssExtractPlugin({
             filename: "estilo.css"
         })
     ],
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true
+            }),
+            new OptimizeAssetsPluguin()
+        ]
+    },
     module: {
     rules: [{
         test: /\.s?[ac]ss$/,
@@ -22,6 +39,9 @@ module.exports = {
             "css-loader",
             "sass-loader",
         ],
+    }, {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']    
     }],
 }
 };
